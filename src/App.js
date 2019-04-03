c102a38f8b0c2974c0f854f757c773a78e09de88
Illia Olenchenko <icrosil@gmail.com>
2019-04-03 22:14:38 +0300

    react-d3 amend

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Lotus from './component/Lotus';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Lotus />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Lotus />
+  </div>
+);
 
 export default App;
