363fd41af51a4dea16f186f1e6aacf6a03c52236
Illia Olenchenko <icrosil@gmail.com>
2019-04-14 16:43:34 +0300

    feat: added github corner

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import React, { useState } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React from 'react';
+import GithubCorner from 'react-github-corner';
 
 import './App.css';
 import TimeLine from './component/TimeLine';
-import Lotus from './component/Lotus';
-import Footer from './component/Footer';
 
 const App = () => {
-  const [tabOpen, setTabOpen] = useState(0);
-
-  function onTabClick(event, newValue) {
-    setTabOpen(newValue);
-  }
   return (
-    <section className="App-wrapper">
-      <AppBar position="static">
-        <Tabs
-          value={tabOpen}
-          onChange={onTabClick}
-          variant="scrollable"
-          scrollButtons="off"
-        >
-          <Tab icon={<Lotus />} />
-        </Tabs>
-      </AppBar>
-      <div className="App-content">
-        <TimeLine />
-      </div>
-      <Footer />
+    <section className="App">
+      <GithubCorner href="https://github.com/username/repo" />
+      <TimeLine />
     </section>
   );
 };
