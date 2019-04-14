2f1ff1f0bd18c495b88a53604aa5fc56ed51e537
Illia Olenchenko <icrosil@gmail.com>
2019-04-14 16:13:13 +0300

    feat: added lotus icon as tab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import './App.css';
 import TimeLine from './component/TimeLine';
+import Lotus from './component/Lotus';
+import Footer from './component/Footer';
 
-const App = () => (
-  <div className="App">
-    <TimeLine />
-  </div>
-);
+const App = () => {
+  const [tabOpen, setTabOpen] = useState(0);
+
+  function onTabClick(event, newValue) {
+    setTabOpen(newValue);
+  }
+  return (
+    <section className="App-wrapper">
+      <AppBar position="static">
+        <Tabs
+          value={tabOpen}
+          onChange={onTabClick}
+          variant="scrollable"
+          scrollButtons="off"
+        >
+          <Tab icon={<Lotus />} />
+        </Tabs>
+      </AppBar>
+      <div className="App-content">
+        <TimeLine />
+      </div>
+      <Footer />
+    </section>
+  );
+};
 
 export default App;
